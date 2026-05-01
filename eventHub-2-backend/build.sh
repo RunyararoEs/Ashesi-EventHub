3d6c8252b5087ba003e7e5cd0be681b805bce728
EdudziNyaho <edudzinyaho@gmail.com>
2026-05-01 18:16:14 +0000

    Add superuser creation to build script

--- eventHub-2-backend/build.sh
+++ eventHub-2-backend/build.sh
@@ -4,3 +4,4 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(email='admin@eventhub.com').exists() or User.objects.create_superuser(email='admin@eventhub.com', username='SystemAdmin', password='Admin1234!', role='system_admin')" | python manage.py shell
